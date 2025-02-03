5bff18ec3bf1abd0b40b5921f3250d648b65c854
Manobala Vijayakumar <47525647+MaN0BaLa@users.noreply.github.com>
2025-02-03 23:56:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ function showMessage() {
 
     // Redirect to external link when clicked
     linkButton.onclick = function() {
-        window.location.href = "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZzVpdTcxNzFsd2VjNWk0emI5cmt0aHJnejEyY2U3dWE4Z2g4Y21ucCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/JdBVId6GmYIpjMSi4J/giphy.gif"; // Replace with your actual link
+        window.location.href = "https://i.pinimg.com/originals/71/43/05/714305bab62625a16fd2a17340ad06f3.gif"; // Replace with your actual link
     };
 
     // Append button below the message
